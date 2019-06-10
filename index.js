f172b7a2e297d8ec84313dab92e699791e75f085
VinhTV7 <vinhtran.6686@gmail.com>
2019-06-10 20:21:42 +0700

    Require cat module in index.js

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-var Dog = require('./Dog');
-var cat = require('./cat');
 
-var dog = new Dog('tom');
-dog.Say();
-var MEO  = new cat();
-dog.Eat(MEO);
-console.log(dog);
+var cat = require('./cat');
